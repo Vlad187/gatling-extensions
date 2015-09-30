929bde548b042d6a47b20c7f8742f21c3e110a35
OBFurman <1234qwer>
2015-09-30 15:59:20 +0300

    Added <br> to output.

--- src/main/scala/debug/RequestExtraInfo.scala
+++ src/main/scala/debug/RequestExtraInfo.scala
@@ -1,23 +1,24 @@
 package debug
 
 import io.gatling.core.result.message.KO
-import io.gatling.http.request.builder.HttpRequestBuilder
+import io.gatling.http.request.builder.HttpRequestWithParamsBuilder
+
 import scala.collection.mutable
 
-class RequestExtraInfo(val extraInfoMap: mutable.HashMap[String, String] = new mutable.HashMap[String, String]()) {
+class RequestExtraInfo(val extraInfoMap: mutable.HashMap[String, String] = new mutable.HashMap[String, String]() with scala.collection.mutable.SynchronizedMap[String, String]) {
 
-  implicit class Util(requestBuilder: HttpRequestBuilder) {
+  implicit class ExtraInfoUtils(requestBuilder: HttpRequestWithParamsBuilder) {
 
-    def saveFirstKoExtraInfo(): HttpRequestBuilder = requestBuilder.extraInfoExtractor(
+    def saveFirstKoExtraInfo(): HttpRequestWithParamsBuilder = requestBuilder.extraInfoExtractor(
       extraInfo => {
         val key = s"${extraInfo.request.getUrl}${extraInfo.response.statusCode.getOrElse(0)}${extraInfo.requestName}"
-        extraInfo.status == KO || extraInfo.response.statusCode.getOrElse(0) != 200 && extraInfoMap.contains(key).unary_! match {
+        extraInfo.status == KO && extraInfoMap.contains(key).unary_! match {
           case true =>
             extraInfoMap.put(
               key,
-              s"Request info:\n ${extraInfo.request.toString.replaceAll("\t", "\n ")}\n\n" +
-                s"Response info:\n ${extraInfo.response.toString.replaceAll("\t", "\n ")}\n\n\n" +
-                s"${extraInfo.session}"
+              "<br><br>" + s"Request info:\n ${extraInfo.request.toString.replaceAll("\t", "\n ")}\n\n" +
+                "<br><br>" + s"Response info:\n ${extraInfo.response.toString.replaceAll("\t", "\n ")}\n\n\n" +
+                "<br><br>" + s"${extraInfo.session}"
             )
             List(key, extraInfoMap(key))
           case false => Nil
